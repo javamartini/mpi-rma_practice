6c37a5bcdd9b6c7790a4e9350b97e9a4f086cc50
Javontae Alexander Martin <jamartin43@tntech.edu>
2025-11-25 14:19:48 -0600

    Remove notice logger function

--- mpi-rma_practice/include/io.hpp
+++ mpi-rma_practice/include/io.hpp
@@ -5,12 +5,7 @@
 #include "comm.hpp"
 #include <mpi.h>
 
-#include <print>
 #include <string>
-#include <cstdarg>
-
-// Log to both the CLI and a log-file.
-int log(MPI_File* file, WorldInfo* info, int type, std::string& format, ...);
 
 // Write execution time data of each process to a file.
 int write_execution_time(MPI_File* file, WorldInfo* info, std::string& op,
