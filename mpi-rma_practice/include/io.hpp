274366e43e753333b392f17fdbe7671c7eca672d
Javontae Alexander Martin <jamartin43@tntech.edu>
2025-11-25 17:48:56 -0600

    Add function prototype for creating data file

--- mpi-rma_practice/include/io.hpp
+++ mpi-rma_practice/include/io.hpp
@@ -5,10 +5,17 @@
 #include "comm.hpp"
 #include <mpi.h>
 
+#include <filesystem>
+#include <print>
 #include <string>
 
+namespace filesys = std::filesystem;
+
+// Ensure the data directory is present for data storage.
+int ensure_data_dir(WorldInfo* info, filesys::path& file_path);
+
 // Write execution time data of each process to a file.
-int write_execution_time(MPI_File* file, WorldInfo* info, std::string& op,
+int write_execution_time(WorldInfo* info, MPI_File* file, std::string& op,
 	double* exec_time);
 
 #endif // IP_HPP
