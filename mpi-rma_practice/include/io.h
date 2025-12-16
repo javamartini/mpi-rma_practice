543e34cff9ae74bc1935fe16363fbe4c4b6891ea
Javontae Alexander Martin <jamartin43@tntech.edu>
2025-12-16 11:18:47 -0500

    Modify IO header and creation of data dir from C++ to C

---
+++ mpi-rma_practice/include/io.h
@@ -0,0 +1,15 @@
+#ifndef IO_H
+#define IO_H
+
+#include <stdbool.h>
+
+struct WorldInfo;   // Forward declaration of MPI world info.
+
+// Ensure the data directory is present for data storage.
+int ensure_data_dir(WorldInfo* info);
+
+// Write execution time data of each process to a file.
+int write_execution_time(WorldInfo* info, MPI_File* file, const char* op,
+                         double* exec_time);
+
+#endif // IP_HPP
---
+++ mpi-rma_practice/src/io.c
@@ -0,0 +1,40 @@
+#include "io.h"
+#include "comm.h"
+
+#include <mpi.h>  // For inter-process communication and file I/O.
+#include <stdio.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <string.h>
+#include <errno.h>
+
+// For filesystem traversal and operations.
+#include <dirent.h>
+#include <sys/stat.h>
+
+// Ensure the data directory is present.
+int ensure_data_dir(WorldInfo* info, const char* data_dir) {
+  if (info->rank != 0) {
+    return 0;
+  }
+
+  int rc = -1;
+  bool found_dir = false;
+  //struct dirent* dir_info;
+  DIR* dir = opendir(data_dir);
+
+  if (dir != NULL) {
+    found_dir = true;
+    closedir(dir);
+  }
+
+  if (!found_dir) {
+    rc = mkdir(data_dir, S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH);
+    if (rc != 0) {
+      perrno("mkdir()");
+      return 1;
+    }
+  }
+
+  return 0;
+}
