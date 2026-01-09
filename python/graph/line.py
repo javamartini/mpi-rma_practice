8569742a785a657cae4ca956276c6a329e31ff53
Javontae Alexander Martin <jamartin43@tntech.edu>
2026-01-09 00:34:47 -0500

    Add initial Python directory structure

---
+++ python/graph/line.py
@@ -0,0 +1,9 @@
+from ..utils import io
+import csv
+import matplotlib.pyplot as matplot
+
+def main():
+    io.find_data_dir()
+
+if __name__ == "__main__":
+    main()
---
+++ python/utils/io.py
@@ -0,0 +1,5 @@
+import os
+
+def find_data_dir():
+    home_dir = os.path.expanduser("~")
+    print(home_dir)
